befd9c8ae8b7001c4c916b50ef466e47bba28343
code-project <dev@code-check.io>
2016-12-04 09:27:04 +0000

    Initial commit

---
+++ app/hello.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = function hello(name) {
+/*
+* ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
+* nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
+*/
+  //ここにHelloworldを実装します。
+  return "Hello " + name + "!";
+};
